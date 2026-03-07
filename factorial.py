4723abb23d4bc13314a4f82d998ad0ad72765123
Sanjana Gupta <sanjana9867199102@gmail.com>
2026-03-07 20:37:06 +0530

    Added Python practice programs
    Basic Python practice program like factorial , palindrome and prime number.

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your number : "))
+
+fact = 1
+
+for i in range(1,num+1):
+    fact = fact * i
+
+print("factorial =",fact )
---
+++ prime_number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter your number :"))
+
+count = 0
+
+for i in range(1,num+1):
+    if num % i == 0:
+        count += 1
+
+if count == 2:
+    print("Prime number")
+else:
+    print("not prime number")
